4b34e451f02eeede49ebfe6eb710b395bc8da789
Alexis Carlier <alexis.carlier0@gmail.com>
2017-11-28 14:51:55 +0000

    Refatored tests to test return value rather than state

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -8,46 +8,42 @@ describe("Gilded Rose", function() {
     const gildedRose = new Shop([ item ]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toEqual("foo");
+    // this test is different from others. Keeping as comparison for when reviewing
   });
 
   it("decreaes the sell in and quality for normal items", function() {
-    item = { name: "Fake item", sellIn: 1, quality: 5 }
+    item = { name: "Fake Item", sellIn: 1, quality: 5 }
+    newItem = { name: "Fake Item", sellIn: 0, quality: 4}
     const gildedRose = new Shop([ item ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].sellIn).toEqual(0);
-    expect(items[0].quality).toEqual(4);
+    expect(gildedRose.updateQuality()).toContain(newItem);
   });
 
   it("increases in quality if is Aged Brie", function() {
     item = { name: "Aged Brie", sellIn: 3, quality: 10 }
+    newItem = { name: "Aged Brie", sellIn: 2, quality: 11}
     const gildedRose = new Shop([ item ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toEqual(11);
-    expect(items[0].sellIn).toEqual(2);
+    expect(gildedRose.updateQuality()).toContain(newItem);
   });
 
   it("when passes sell by date, quality ofincreaes twice as fast Aged Brie", function() {
-    item = { name: "Aged Brie", sellIn: -1, quality: 10 }
+    item = { name: "Aged Brie", sellIn: -1, quality: 10}
+    newItem = { name: "Aged Brie", sellIn: -2, quality: 12}
     const gildedRose = new Shop([item]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toEqual(12);
-    expect(items[0].sellIn).toEqual(-2);
+    expect(gildedRose.updateQuality()).toContain(newItem);
   });
 
   it("for a normal item, qualit degrades twice as fast when past sell by date", function() {
-    item = { name: "Fake item", sellIn: -1, quality: 10 }
+    item = { name: "Fake Item", sellIn: -1, quality: 10 }
+    newItem = { name: "Fake Item", sellIn: -2, quality: 8}
     const gildedRose = new Shop([ item ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toEqual(8);
-    expect(items[0].sellIn).toEqual(-2);
+    expect(gildedRose.updateQuality()).toContain(newItem);
   });
 
   it("an item cannot have quality of more than 50", function() {
     item = { name: "Aged Brie", sellIn: -1, quality: 50 }
+    newItem = { name: "Aged Brie", sellIn: -2, quality: 50}
     const gildedRose = new Shop([item]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toEqual(50);
-    expect(items[0].sellIn).toEqual(-2);
+    expect(gildedRose.updateQuality()).toContain(newItem);
   });
 
   // it("Works for Sulfuras behaviour", function() {
