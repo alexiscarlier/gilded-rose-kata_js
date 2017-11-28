549c4a7bfbd40cdf0ae8bdd33acad884011bdb3f
Alexis Carlier <alexis.carlier0@gmail.com>
2017-11-28 14:19:07 +0000

    Doubled item in guilded rose spec using hashes

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -4,43 +4,63 @@ var Item = require('../src/item.js');
 describe("Gilded Rose", function() {
 
   it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    item = { name: "foo", sellIn: 0, quality: 0 }
+    const gildedRose = new Shop([ item ]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toEqual("foo");
   });
 
   it("decreaes the sell in and quality for normal items", function() {
-    const gildedRose = new Shop([ new Item("Fake item", 1, 5)]);
+    item = { name: "Fake item", sellIn: 1, quality: 5 }
+    const gildedRose = new Shop([ item ]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toEqual(0);
     expect(items[0].quality).toEqual(4);
   });
 
   it("increases in quality if is Aged Brie", function() {
-    const gildedRose = new Shop([ new Item("Aged Brie", 3, 10)]);
+    item = { name: "Aged Brie", sellIn: 3, quality: 10 }
+    const gildedRose = new Shop([ item ]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(11);
     expect(items[0].sellIn).toEqual(2);
   });
 
   it("when passes sell by date, quality ofincreaes twice as fast Aged Brie", function() {
-    const gildedRose = new Shop([ new Item("Aged Brie", -1, 10)]);
+    item = { name: "Aged Brie", sellIn: -1, quality: 10 }
+    const gildedRose = new Shop([item]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(12);
     expect(items[0].sellIn).toEqual(-2);
   });
 
   it("for a normal item, qualit degrades twice as fast when past sell by date", function() {
-    const gildedRose = new Shop([ new Item("Fake item", -1, 10)]);
+    item = { name: "Fake item", sellIn: -1, quality: 10 }
+    const gildedRose = new Shop([ item ]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(8);
     expect(items[0].sellIn).toEqual(-2);
   });
 
   it("an item cannot have quality of more than 50", function() {
-    const gildedRose = new Shop([ new Item("Aged Brie", -1, 50)]);
+    item = { name: "Aged Brie", sellIn: -1, quality: 50 }
+    const gildedRose = new Shop([item]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(50);
     expect(items[0].sellIn).toEqual(-2);
   });
+
+  // it("Works for Sulfuras behaviour", function() {
+  //   const gildedRose = new Shop([ new Item("Sulfuras", 0, 50)]);
+  //   const items = gildedRose.updateQuality();
+  //   expect(items[0].quality).toEqual(50);
+  //   expect(items[0].sellIn).toEqual(0);
+  // });
+
+  // it("Backstage passes", function() {
+  //   const gildedRose = new Shop([ new Item("Backstage passes", 30, 30)]);
+  //   const items = gildedRose.updateQuality();
+  //   expect(items[0].quality).toEqual(50);
+  //   expect(items[0].sellIn).toEqual(0);
+  // });
 });
