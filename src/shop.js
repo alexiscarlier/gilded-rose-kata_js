25216c5fc106119ca9c98a0ed8eb157d9aeef806
Alexis Carlier <alexis.carlier0@gmail.com>
2017-11-28 18:33:14 +0000

    Added quality to zero and decrement sell in functions

--- src/shop.js
+++ src/shop.js
@@ -11,6 +11,16 @@ class Shop {
     item.quality += 1;
   }
 
+  qualityToZero(item) {
+    item.quality = 0;
+    // original implementation of this method was the below
+    // this.items[i].quality = this.items[i].quality - this.items[i].quality;
+  }
+
+  decrementSellIn(item) {
+    item.sellIn -= 1;
+  }
+
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -21,7 +31,7 @@ class Shop {
         }
       } else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+          this.incrementQuality(this.items[i]);
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
@@ -30,29 +40,29 @@ class Shop {
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.incrementQuality(this.items[i]);
               }
             }
           }
         }
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        this.decrementSellIn(this.items[i]);
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+                this.decrementQuality(this.items[i]);
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.qualityToZero(this.items[i]);
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+            this.incrementQuality(this.items[i]);
           }
         }
       }
