1a229061ef3a878c004f7e1b304cfa9c15e03cb1
Alexis Carlier <alexis.carlier0@gmail.com>
2017-11-30 10:19:56 +0000

    Got backstage pass function working properly

--- src/shop.js
+++ src/shop.js
@@ -43,10 +43,6 @@ class Shop {
     return item.name === 'Aged Brie';
   }
 
-  isSulfurus(item) {
-    return item === 'Sulfuras, Hand of Ragnaros';
-  }
-
   isBackstagePass(item) {
     return item.name === 'Backstage passes to a TAFKAL80ETC concert';
   }
@@ -63,11 +59,11 @@ class Shop {
   }
 
   backstagePassUpdate(item) {
-    // if (item.sellIn < 0) {
-    //   this.decrementSellIn(item);
-    //   this.qualityToZero(item);
-    // }
-    if (item.sellIn < 6) {
+    if (item.sellIn <= 0) {
+      this.decrementSellIn(item);
+      this.qualityToZero(item);
+    }
+    else if (item.sellIn < 6) {
       this.decrementSellIn(item);
       item.quality += 3;
     }
@@ -82,15 +78,7 @@ class Shop {
     if (item.quality > 50) {
       item.quality = 50;
     }
-    // if (item.sellIn < 0) {
-    //   this.decrementSellIn(item);
-    //   this.qualityToZero(item);
-    // }
   }
-
-  // sulfurusUpdate(item) {
-  //   item;
-  // }
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
@@ -103,9 +91,6 @@ class Shop {
       if (this.isBackstagePass(this.items[i])) {
         this.backstagePassUpdate(this.items[i]);
       }
-      // if(this.isSulfurus(this.items[i])) {
-      //   this.sulfurusUpdate(this.items[i]);
-      // }
     }
     return this.items;
   }
@@ -113,113 +98,3 @@ class Shop {
 }
 
 module.exports = Shop;
-
-//   decrementQuality(item) {
-//     item.quality -= 1;
-//   }
-//
-//   incrementQuality(item) {
-//     item.quality += 1;
-//   }
-//
-//   qualityToZero(item) {
-//     item.quality = 0;
-//     // original implementation of this method was the below
-//     // this.items[i].quality = this.items[i].quality - this.items[i].quality;
-//   }
-//
-//   decrementSellIn(item) {
-//     item.sellIn -= 1;
-//   }
-//
-//   // normalUpdate(item) {
-//   //
-//   // }
-//
-//   normalUpdateQuality(item) {
-//     if (item.sellIn < 0 && item.quality > 0) {
-//       item.quality -= 2;
-//     }
-//     else if (item.quality > 0) {
-//       this.decrementQuality(item);
-//     }
-//     if (item.quality < 0) {
-//       this.qualityToZero(item);
-//     }
-//   }
-//
-//   normalUpdateSellIn(item) {
-//     this.decrementSellIn(item);
-//   }
-//
-//   backstageUpdateQuality(item) {
-//     if (item.sellIn < 6 && item.quality < 50) {
-//       item.quality += 2;
-//     }
-//     else if (item.sellIn < 11 && item.quality < 50) {
-//       this.incrementQuality(item);
-//     }
-//   }
-//
-//   isBackstageItem(item) {
-//     return item.name == 'Backstage passes to a TAFKAL80ETC concert';
-//   }
-//
-//   isNormalItem(item) {
-//     return this.namesOfExceptions.includes(item.name) === false;
-//   }
-//
-//   updateQuality() {
-//     for (var i = 0; i < this.items.length; i++)
-//       if (this.isNormalItem(this.items[i]) {
-//         normalUpdateQuality(this.items[i]);
-//       }
-//   }
-// }
-
-
-
-
-
-
-
-//   updateQuality() {
-//     for (var i = 0; i < this.items.length; i++) {
-//       if (this.isNormalItem(this.items[i])) {
-//         this.normalUpdateQuality(this.items[i]);
-//         // this.normalUpdateSellIn(this.items[i]);
-//       } else {
-//         if (this.items[i].quality < 50) {
-//           this.incrementQuality(this.items[i]);
-//           if (this.isBackstageItem(this.items[i])) {
-//             this.backstageUpdateQuality(this.items[i]);
-//           }
-//         }
-//       }
-//       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-//         this.normalUpdateSellIn(this.items[i]);
-//       }
-//       if (this.items[i].sellIn < 0) {
-//         if (this.items[i].name != 'Aged Brie') {
-//           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-//             if (this.items[i].quality > 0) {
-//               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-//                 this.decrementQuality(this.items[i]);
-//               }
-//             }
-//           }
-//           else {
-//             this.qualityToZero(this.items[i]);
-//           }
-//         } else {
-//           if (this.items[i].quality < 50) {
-//             this.incrementQuality(this.items[i]);
-//           }
-//         }
-//       }
-//     }
-//
-//     return this.items;
-//   }
-//
-// }
