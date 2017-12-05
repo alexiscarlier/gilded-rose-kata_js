2fa0a5b3997b885979347554665368a036a345fd
Alexis Carlier <alexis.carlier0@gmail.com>
2017-12-05 19:05:54 +0000

    Minor refactoring of normal item class

--- src/normalItem.js
+++ src/normalItem.js
@@ -2,15 +2,13 @@ var Item = require('../src/item.js');
 
 class NormalItem extends Item {
   update() {
-    this.quality -= 1;
+    this.sellIn < 0 ? this.quality -= 2 : this.quality -= 1;
     this.sellIn -= 1;
-    if (this.sellIn < 0) {
-      this.quality -= 1;
-    }
     if (this.quality < 0) {
       this.quality = 0;
     }
   }
+
 }
 
 module.exports = NormalItem;
